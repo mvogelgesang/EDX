a2eb4204d99c0c5752ed582f4cc90cb633c043b0
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2022-04-29 00:19:47 -0400

    #378 greyed out null/zero data, requires cleanup but working

--- Tools/scanner/src/evals.ts
+++ Tools/scanner/src/evals.ts
@@ -3,13 +3,43 @@ const yaml = require("js-yaml");
 const fs = require("fs").promises;
 import * as utils from "./utils";
 const formattedDate = utils.getFormattedDate();
-const FIELD_MAP = {
+const AIRTABLE_TABLES = {
+  websites: "tblFUfp0KQibuCTeb",
+  "website-evals": "tblEc5CVjvlElEz8l",
+};
+const AIRTABLE_FIELDS = {
   "website-evals": {
-    Site: "fldEJKjLyRz34tQzh",
+    Site: { id: "fldEJKjLyRz34tQzh", name: "Site" },
+  },
+  websites: {
+    website: { id: "fldFrU6QZcwAdraF7", name: "Site" },
+    active: { id: "fldnjl7wNChb5d6Vu", name: "Active" },
+    office: { id: "fldF6peALqDlbDlBp", name: "Office" },
+    "sub-office": { id: "fldAsCVsayge6mxhK", name: "Sub-Office" },
+    "website-platform": { id: "fldOEB2p3zzQJUfX8", name: "" },
+    "website-category": { id: "fld76C5VCbbfQ0w4v", name: "Website Category" },
+    "score-customer-centricity": {
+      id: "fldWhw3jTSLJzbbx4",
+      name: "Statuscard Score - Customer Centricity",
+    },
+    "score-mobile-perf": {
+      id: "fldfvDZZqPnb2qbsG",
+      name: "Statuscard Score - Mobile Performance Rollup (from USWDS, Performance)",
+    },
+    "score-amp": { id: "fldN3wy0EtgQ1aU4w", name: "Statuscard Score - AMP" },
+    "score-ga": { id: "fldkwF5Zusr17tBcu", name: "Statuscard Score - GA data" },
+    "score-uswds": {
+      id: "fld6u0AYPH52m19iP",
+      name: "Statuscard Score - USWDS",
+    },
+    "score-reqd-links": {
+      id: "fldfQdluzWsPP2aty",
+      name: "Statuscard Score - Required Links",
+    },
   },
 };
 const AIRTABLE_BASE = "appaxAzqTVnbOf7cm";
-const WEBSITE_EVAL_TABLE = "tblEc5CVjvlElEz8l";
+
 const base = require("airtable").base(AIRTABLE_BASE);
 var Airtable = require("airtable");
 Airtable.configure({
@@ -38,17 +68,31 @@ const platforms = [
 ///////////////////////
 
 let data = [];
-base(WEBSITE_EVAL_TABLE)
+base(AIRTABLE_TABLES.websites)
   .select({
+    fields: [
+      AIRTABLE_FIELDS.websites.website.id,
+      AIRTABLE_FIELDS.websites.office.id,
+      AIRTABLE_FIELDS.websites["sub-office"].id,
+      AIRTABLE_FIELDS.websites["website-platform"].id,
+      AIRTABLE_FIELDS.websites["website-category"].id,
+      AIRTABLE_FIELDS.websites["score-amp"].id,
+      AIRTABLE_FIELDS.websites["score-customer-centricity"].id,
+      AIRTABLE_FIELDS.websites["score-ga"].id,
+      AIRTABLE_FIELDS.websites["score-mobile-perf"].id,
+      AIRTABLE_FIELDS.websites["score-reqd-links"].id,
+      AIRTABLE_FIELDS.websites["score-uswds"].id,
+    ],
+    sort: [{ field: AIRTABLE_FIELDS.websites.website.id, direction: "asc" }],
     // Selecting the first 3 records in Grid view:
-    view: "Grid view",
+    view: "Active Sites",
   })
   .eachPage(
     function page(records, fetchNextPage) {
       // This function (`page`) will get called for each page of records.
 
       records.forEach(function (record) {
-        data.push(genDummyData(record.get("Site")));
+        data.push(organizeResult(record));
       });
 
       // To fetch the next page of records, call `fetchNextPage`.
@@ -79,6 +123,42 @@ base(WEBSITE_EVAL_TABLE)
       });
     }
   );
+
+const organizeResult = function (record) {
+  return {
+    name: record.fields[AIRTABLE_FIELDS.websites.website.name],
+    office: record.fields[AIRTABLE_FIELDS.websites.office.name],
+    subOffice: record.fields[AIRTABLE_FIELDS.websites["sub-office"].name],
+    category: record.fields[AIRTABLE_FIELDS.websites["website-category"].name],
+    platform: platforms[getRandNo(7)],
+    datapointA: {
+      val: record.fields[
+        AIRTABLE_FIELDS.websites["score-customer-centricity"].name
+      ],
+      max: 1,
+    },
+    datapointB: {
+      val: record.fields[AIRTABLE_FIELDS.websites["score-reqd-links"].name],
+      max: 1,
+    },
+    datapointC: {
+      val: record.fields[AIRTABLE_FIELDS.websites["score-mobile-perf"].name],
+      max: 1,
+    },
+    datapointD: {
+      val: record.fields[AIRTABLE_FIELDS.websites["score-amp"].name],
+      max: 1,
+    },
+    datapointE: {
+      val: record.fields[AIRTABLE_FIELDS.websites["score-ga"].name],
+      max: 1,
+    },
+    datapointF: {
+      val: record.fields[AIRTABLE_FIELDS.websites["score-uswds"].name],
+      max: 1,
+    },
+  };
+};
 
 const genDummyData = function (siteName) {
   return {
