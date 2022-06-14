3e5d3e4a875191b352a0d31e281eae25a889cee3
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2022-06-14 09:13:32 -0400

    Issue 138 (#448)
    * #138 finalizing work to manage a list of preset condenser exports. No ability to toggle between them at the CLI layer, will do so when it is merged into edxcli

--- Tools/scanner/src/condenser/condenser.module.ts
+++ Tools/scanner/src/condenser/condenser.module.ts
@@ -5,6 +5,8 @@ const fs = require("fs").promises;
 import path from "path";
 import _ from "lodash";
 import * as utils from "../utils";
+import { headerConfigurations } from "./csvHeaders";
+
 let args = require("minimist")(process.argv.slice(2), {
   string: "folders",
   alias: { f: "folders" },
@@ -12,86 +14,8 @@ let args = require("minimist")(process.argv.slice(2), {
 
 const formattedDate = utils.getFormattedDate("YYYYMMDD_HHMM");
 export const outputPath = `data/output_${formattedDate}.csv`;
-const csvHeaders = [
-  { id: "domain", title: "Domain" },
-  { id: "url", title: "URL" },
-  { id: "scanDate", title: "Scan Date" },
-  { id: "scanVersion", title: "Scan Version" },
-  { id: "scanStatus", title: "Scan Status" },
-  { id: "performanceMetric.hsts", title: "HSTS" },
-  { id: "performanceMetric.dap", title: "Digital Analytics (DAP)" },
-  { id: "performanceMetric.contact", title: "Contact" },
-  { id: "performanceMetric.banner", title: "USA Banner" },
-  { id: "performanceMetric.identifier", title: "Identifier" },
-  {
-    id: "performanceMetric.identifierAccessibility",
-    title: "Accessibility Link",
-  },
-  { id: "performanceMetric.identifierFOIA", title: "FOIA Link" },
-  { id: "performanceMetric.identifierPrivacy", title: "Privacy Policy Link" },
-  { id: "performanceMetric.search", title: "Search" },
-  { id: "siteScanner.data.scan_date", title: "Site Scanner Date" },
-  { id: "siteScanner.data.uswds_usa_classes", title: "USA Class Count" },
-  { id: "siteScanner.data.uswds_semantic_version", title: "USWDS Version" },
-  { id: "uswdsComponents.accordion", title: "USWDS Accordion" },
-  { id: "uswdsComponents.alert", title: "USWDS Alert" },
-  { id: "uswdsComponents.button", title: "USWDS Button" },
-  { id: "uswdsComponents.card", title: "USWDS Card" },
-  { id: "uswdsComponents.footer", title: "USWDS Footer" },
-  { id: "uswdsComponents.header", title: "USWDS Header" },
-  {
-    id: "lighthouse.desktopData.lhr.categories.performance.score",
-    title: "(D) Performance Score",
-  },
-  {
-    id: "lighthouse.desktopData.lhr.audits['speed-index'].score",
-    title: "(D) Speed Index",
-  },
-  {
-    id: "lighthouse.desktopData.lhr.audits['content-width'].score",
-    title: "(D) Content Width Score",
-  },
-  {
-    id: "lighthouse.desktopData.lhr.categories.seo.score",
-    title: "(D) SEO Score",
-  },
-  {
-    id: "lighthouse.mobileData.lhr.categories.performance.score",
-    title: "(M) Performance Score",
-  },
-  {
-    id: "lighthouse.mobileData.lhr.audits['speed-index'].score",
-    title: "(M) Speed Index Score",
-  },
-  {
-    id: "lighthouse.mobileData.lhr.audits['content-width'].score",
-    title: "(M) Content Width Score",
-  },
-  {
-    id: "lighthouse.mobileData.lhr.categories.seo.score",
-    title: "(M) SEO Score",
-  },
-  {
-    id: "lighthouse.desktopData.lhr.audits['content-width'].score",
-    title: "Page Content Width Set",
-  },
-  {
-    id: "lighthouse.desktopData.lhr.audits['meta-viewport'].score",
-    title: "Meta Viewport Set",
-  },
-  {
-    id: "lighthouse.desktopData.lhr.audits['meta-description'].score",
-    title: "Meta Description Set",
-  },
-  {
-    id: "lighthouse.desktopData.lhr.audits['document-title'].score",
-    title: "Document Title Set",
-  },
-  {
-    id: "lighthouse.desktopData.lhr.audits['html-has-lang'].score",
-    title: "HTML Language Set",
-  },
-];
+const csvHeaders = headerConfigurations.default;
+
 const csvWriter = createCsvWriter({
   path: outputPath,
   // create map of csv headers to json elements
---
+++ Tools/scanner/src/condenser/csvHeaders.ts
@@ -0,0 +1,242 @@
+export const headerConfigurations = {
+  default: [
+    { id: "domain", title: "Domain" },
+    { id: "url", title: "URL" },
+    { id: "scanDate", title: "Scan Date" },
+    { id: "scanVersion", title: "Scan Version" },
+    { id: "scanStatus", title: "Scan Status" },
+    { id: "performanceMetric.hsts", title: "HSTS" },
+    { id: "performanceMetric.dap", title: "Digital Analytics (DAP)" },
+    { id: "performanceMetric.contact", title: "Contact" },
+    { id: "performanceMetric.banner", title: "USA Banner" },
+    { id: "performanceMetric.identifier", title: "Identifier" },
+    {
+      id: "performanceMetric.identifierAccessibility",
+      title: "Accessibility Link",
+    },
+    { id: "performanceMetric.identifierFOIA", title: "FOIA Link" },
+    { id: "performanceMetric.identifierPrivacy", title: "Privacy Policy Link" },
+    { id: "performanceMetric.search", title: "Search" },
+    { id: "siteScanner.data.scan_date", title: "Site Scanner Date" },
+    { id: "siteScanner.data.uswds_usa_classes", title: "USA Class Count" },
+    { id: "siteScanner.data.uswds_semantic_version", title: "USWDS Version" },
+    { id: "uswdsComponents.accordion", title: "USWDS Accordion" },
+    { id: "uswdsComponents.alert", title: "USWDS Alert" },
+    { id: "uswdsComponents.button", title: "USWDS Button" },
+    { id: "uswdsComponents.card", title: "USWDS Card" },
+    { id: "uswdsComponents.footer", title: "USWDS Footer" },
+    { id: "uswdsComponents.header", title: "USWDS Header" },
+    {
+      id: "lighthouse.desktopData.lhr.categories.performance.score",
+      title: "(D) Performance Score",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['speed-index'].score",
+      title: "(D) Speed Index",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['content-width'].score",
+      title: "(D) Content Width Score",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.categories.seo.score",
+      title: "(D) SEO Score",
+    },
+    {
+      id: "lighthouse.mobileData.lhr.categories.performance.score",
+      title: "(M) Performance Score",
+    },
+    {
+      id: "lighthouse.mobileData.lhr.audits['speed-index'].score",
+      title: "(M) Speed Index Score",
+    },
+    {
+      id: "lighthouse.mobileData.lhr.audits['content-width'].score",
+      title: "(M) Content Width Score",
+    },
+    {
+      id: "lighthouse.mobileData.lhr.categories.seo.score",
+      title: "(M) SEO Score",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['content-width'].score",
+      title: "Page Content Width Set",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['meta-viewport'].score",
+      title: "Meta Viewport Set",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['meta-description'].score",
+      title: "Meta Description Set",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['document-title'].score",
+      title: "Document Title Set",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['html-has-lang'].score",
+      title: "HTML Language Set",
+    },
+  ],
+  lighthouseAccessibility: [
+    { id: "domain", title: "Domain" },
+    { id: "url", title: "URL" },
+    { id: "scanDate", title: "Scan Date" },
+    { id: "scanVersion", title: "Scan Version" },
+    { id: "scanStatus", title: "Scan Status" },
+    { id: "performanceMetric.hsts", title: "HSTS" },
+    { id: "performanceMetric.dap", title: "Digital Analytics (DAP)" },
+    { id: "performanceMetric.contact", title: "Contact" },
+    { id: "performanceMetric.banner", title: "USA Banner" },
+    { id: "performanceMetric.identifier", title: "Identifier" },
+    {
+      id: "performanceMetric.identifierAccessibility",
+      title: "Accessibility Link",
+    },
+    { id: "performanceMetric.identifierFOIA", title: "FOIA Link" },
+    { id: "performanceMetric.identifierPrivacy", title: "Privacy Policy Link" },
+    { id: "performanceMetric.search", title: "Search" },
+    { id: "siteScanner.data.scan_date", title: "Site Scanner Date" },
+    { id: "siteScanner.data.uswds_usa_classes", title: "USA Class Count" },
+    { id: "siteScanner.data.uswds_semantic_version", title: "USWDS Version" },
+    { id: "uswdsComponents.accordion", title: "USWDS Accordion" },
+    { id: "uswdsComponents.alert", title: "USWDS Alert" },
+    { id: "uswdsComponents.button", title: "USWDS Button" },
+    { id: "uswdsComponents.card", title: "USWDS Card" },
+    { id: "uswdsComponents.footer", title: "USWDS Footer" },
+    { id: "uswdsComponents.header", title: "USWDS Header" },
+    {
+      id: "lighthouse.desktopData.lhr.categories.performance.score",
+      title: "(D) Performance Score",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['speed-index'].score",
+      title: "(D) Speed Index",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['content-width'].score",
+      title: "(D) Content Width Score",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.categories.seo.score",
+      title: "(D) SEO Score",
+    },
+    {
+      id: "lighthouse.mobileData.lhr.categories.performance.score",
+      title: "(M) Performance Score",
+    },
+    {
+      id: "lighthouse.mobileData.lhr.audits['speed-index'].score",
+      title: "(M) Speed Index Score",
+    },
+    {
+      id: "lighthouse.mobileData.lhr.audits['content-width'].score",
+      title: "(M) Content Width Score",
+    },
+    {
+      id: "lighthouse.mobileData.lhr.categories.seo.score",
+      title: "(M) SEO Score",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['content-width'].score",
+      title: "Page Content Width Set",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['meta-viewport'].score",
+      title: "Meta Viewport Set",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['meta-description'].score",
+      title: "Meta Description Set",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['document-title'].score",
+      title: "Document Title Set",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['html-has-lang'].score",
+      title: "HTML Language Set",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['aria-allowed-attr'].score",
+      title: "LH Cat Aria Allowed Attr",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['aria-hidden-body'].score",
+      title: "LH Cat Aria Hidden Body",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['aria-hidden-focus'].score",
+      title: "LH Cat Aria Hidden Focus",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['aria-required-attr'].score",
+      title: "LH Cat Aria Required Attr",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['aria-roles'].score",
+      title: "LH Cat Aria Roles",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['aria-valid-attr-value'].score",
+      title: "LH Cat Aria Valid Attr Value",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['aria-valid-attr'].score",
+      title: "LH Cat Aria Valid Attr",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['button-name'].score",
+      title: "LH Cat Button Name",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['bypass'].score",
+      title: "LH Cat Bypass",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['color-contrast'].score",
+      title: "LH Cat Color Contrast",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['document-title'].score",
+      title: "LH Cat Document Title",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['duplicate-id-aria'].score",
+      title: "LH Cat Duplicate ID Aria",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['heading-order'].score",
+      title: "LH Cat Heading Order",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['html-has-lang'].score",
+      title: "LH Cat HTML Has Lang",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['html-lang-valid'].score",
+      title: "LH Cat HTML Lang Valid",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['image-alt'].score",
+      title: "LH Cat Image Alt",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['link-name'].score",
+      title: "LH Cat Link Name",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['list'].score",
+      title: "LH Cat List",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['listitem'].score",
+      title: "LH Cat Listitem",
+    },
+    {
+      id: "lighthouse.desktopData.lhr.audits['meta-viewport'].score",
+      title: "LH Cat Meta Viewport",
+    },
+  ],
+};
