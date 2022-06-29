54fb1a14ccd80d46db9c56b93c65929594f88a4c
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2022-06-29 16:35:53 -0400

    Issue 402a (#484)
    * #402 updated to remove console statements and added inline comments

--- Tools/edxcli/src/helpers/websites/cui-banner.ts
+++ Tools/edxcli/src/helpers/websites/cui-banner.ts
@@ -23,21 +23,50 @@ export const cuiBanner = async (
     bodyContent = dialog.message();
     await dialog.accept();
   });
+
+  const evaluatePage = async (pageType: string): Promise<void> => {
+    const bodyHandle = await page.$('body');
+    bodyContent += await page.evaluate((body) => body.textContent, bodyHandle);
+    // perform fuzzy search on textcontent
+    warningBanner.push({
+      url: page.url(),
+      external: fuzzy(warninBannerExternalMessage, bodyContent),
+      internal: fuzzy(warningBannerInternalMessage, bodyContent),
+      pageType: pageType,
+    });
+  };
+
+  // go to the home page
   await page.goto(domain.toString());
-  const bodyHandle = await page.$('body');
-  bodyContent += await page.evaluate((body) => body.textContent, bodyHandle);
-  // perform fuzzy search on textcontent
-  warningBanner.push({
-    url: domain.toString(),
-    external: fuzzy(warninBannerExternalMessage, bodyContent),
-    internal: fuzzy(warningBannerInternalMessage, bodyContent),
-  });
+  // evaluate for text
+  await evaluatePage('home');
+  // build an array of links and buttons on the page
+  const linkList = await page.$$('a,button');
+  const loginTerms = /sign in|login|log in/gi;
+  // loop the list of buttons and links to find any containing the regex above
+  for (const element of linkList) {
+    // eslint-disable-next-line no-await-in-loop
+    const valueHandle = await element.getProperty('innerText');
+
+    // when a match is found, click on that element then evaluate the page for warning banner content
+    if (loginTerms.test(valueHandle.toString())) {
+      // eslint-disable-next-line no-await-in-loop
+      await Promise.all([element.click(), page.waitForTimeout(4000)]);
+
+      // eslint-disable-next-line no-await-in-loop
+      await evaluatePage('login');
+      // once the first page has been evaluated, we break out of the loop
+      break;
+    }
+  }
+
   await page.close();
   return warningBanner;
 };
 
 export interface ICuiBanner {
   url?: URL | string;
+  pageType?: string;
   external?: number;
   internal?: number;
 }
