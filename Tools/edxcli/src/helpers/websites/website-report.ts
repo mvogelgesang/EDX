5d4d0cd20842e38e699edfe4201c507a773bc114
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2022-07-05 21:55:15 -0400

    #489 compiling and working as expected.

--- Tools/edxcli/src/helpers/websites/website-report.ts
+++ Tools/edxcli/src/helpers/websites/website-report.ts
@@ -2,6 +2,7 @@
 require('dotenv').config();
 import { ScanHelper } from './scan';
 import { ScreenshotType } from './screenshot';
+import { PerformanceMetricReport } from './it-performance-metric';
 import { ICuiBanner } from './cui-banner';
 import { IMetadataTags } from './metadata-tags';
 
@@ -29,6 +30,18 @@ export const websiteReport = (
         'Holds screenshots of website homepage on both desktop and mobile viewport sizes',
       data: [],
     },
+    performanceMetric: {
+      description: "Represents GSA IT FY22 performance metrics",
+    hsts: false,
+    dap: false,
+    contact: false,
+    banner: false,
+    identifier: false,
+    identifierAccessibility: false,
+    identifierFOIA: false,
+    identifierPrivacy: false,
+    search: false,
+    },
     cuiBanner: {
       description: `Looks for the CUI warning banner on site and returns a fuzzy match ranking between 0 and 1 where a higher score indicates a closer match. Two variants of the CUI warning banner exist and scores are provided for both the internal and external variants. Specific language can be found in source and CIO 2100.1M, https://www.gsa.gov/cdnstatic/CIO_21001M_GSA_Information_Technology_(IT)_Security_Policy_03-26-2021_CC044215.pdf`,
       data: [],
@@ -52,6 +65,7 @@ export type WebsiteReportType = {
   url: string;
   scanStatus: string;
   screenCapture: ScreenshotReport;
+  performanceMetric: PerformanceMetricReport;
   cuiBanner: CuiBannerReport;
   metadataTags: MetadataTagsReport;
 };
