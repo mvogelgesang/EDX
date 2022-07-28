f1de575438a60b67704af20108bd94c42043e927
lilaconlee <lila.conlee@gmail.com>
2022-07-27 21:21:30 -0500

    Fix base URL bugs in production

--- design-consults/astro.config.mjs
+++ design-consults/astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://gsa.github.io",
-  base: "/EDX",
+  base: "/EDX/",
 });
--- design-consults/src/constants.ts
+++ design-consults/src/constants.ts
@@ -1,3 +1,5 @@
 export const SLUGS = ["apps_ocfo_gsa", "contractdirectory"];
 
 export const VIEWS = ["current", "proposed", "diff", "production"];
+
+export const BASE_URL = import.meta.env.BASE_URL;
