1c389e4165ad9d7a8f6654229c0767a9c9f9c338
Lila Conlee <lila.conlee@gmail.com>
2022-08-01 12:33:33 -0500

    Issue 500a fix assets for deployed design consults site (#526)
    * Upgrade Astro to 1.0.0-rc.1
    * Update asset paths in contractdirectory
    * Move temporary production file to public directory
    * Use index.html for public consult assets
    * Remove stale comment

--- design-consults/src/constants.ts
+++ design-consults/src/constants.ts
@@ -1,3 +1,6 @@
 export const SLUGS = ["apps_ocfo_gsa", "contractdirectory"];
 
 export const VIEWS = ["current", "proposed", "diff", "production"];
+
+export const BASE_URL = import.meta.env.BASE_URL;
+export const USWDS_PATH = `${BASE_URL}/vendor/uswds/`;
