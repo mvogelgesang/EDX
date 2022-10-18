7156f504f4604df1b075036cf3b5f5815fe5e62a
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2022-10-18 13:17:12 -0400

    Issue 530 (#531)
    * #530 updating dependencies and tsconfig
    * #530 migrated old json files and ported data-update.ts
    * #530 updated workflow job
    * #530 fixed typo in workflow
    * #530 changing skip_dirty_check so as not to fail if file exists already
    * #530 verified working state locally as this branch is a few months old. Corrected date formatting typo, made updates in airtable

---
+++ design-consults/src/build/data-update.ts
@@ -0,0 +1,159 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+import {writeFile,mkdir} from 'node:fs';
+import Airtable from 'airtable';
+function formattedDate(): string {
+  const date = new Date();
+  return `${date.getFullYear()}${date.getMonth() < 9 ? '0' : ''}${date.getMonth() + 1}${date.getDate() < 10 ? '0' : ''}${date.getDate()}`;
+}
+const AIRTABLE_TABLES = {
+  websites: "tblFUfp0KQibuCTeb",
+  "website-evals": "tblEc5CVjvlElEz8l",
+};
+const AIRTABLE_FIELDS = {
+  "website-evals": {
+    Site: { id: "fldEJKjLyRz34tQzh", name: "Site" },
+  },
+  websites: {
+    website: { id: "fldFrU6QZcwAdraF7", name: "Site" },
+    active: { id: "fldnjl7wNChb5d6Vu", name: "Active" },
+    office: { id: "fldF6peALqDlbDlBp", name: "Office" },
+    "sub-office": { id: "fldAsCVsayge6mxhK", name: "Sub-Office" },
+    "website-platform": { id: "fldOEB2p3zzQJUfX8", name: "" },
+    "digital-brand-category": {
+      id: "fldEJNQ2Bp76EHPPB",
+      name: "Digital Brand Category",
+    },
+    "type-of-domain": { id: "fld76C5VCbbfQ0w4v", name: "Type of Domain" },
+    "score-customer-centricity": {
+      id: "fldWhw3jTSLJzbbx4",
+      name: "Statuscard Score - Customer Centricity",
+    },
+    "score-mobile-perf": {
+      id: "fldfvDZZqPnb2qbsG",
+      name: "Statuscard Score - Mobile Performance Rollup (from USWDS, Performance)",
+    },
+    "score-amp": { id: "fldN3wy0EtgQ1aU4w", name: "Statuscard Score - AMP" },
+    "score-ga": { id: "fldkwF5Zusr17tBcu", name: "Statuscard Score - GA data" },
+    "score-uswds": {
+      id: "fld6u0AYPH52m19iP",
+      name: "Statuscard Score - USWDS",
+    },
+    "score-reqd-links": {
+      id: "fldfQdluzWsPP2aty",
+      name: "Statuscard Score - Required Links",
+    },
+  },
+};
+const AIRTABLE_BASE = "appaxAzqTVnbOf7cm";
+
+
+Airtable.configure({
+  endpointUrl: "https://api.airtable.com",
+  apiKey: `${process.env.AIRTABLE_API_KEY}`,
+});
+const base = Airtable.base(AIRTABLE_BASE);
+
+let data = [];
+base(AIRTABLE_TABLES.websites)
+  .select({
+    fields: [
+      AIRTABLE_FIELDS.websites.website.id,
+      AIRTABLE_FIELDS.websites.office.id,
+      AIRTABLE_FIELDS.websites["sub-office"].id,
+      AIRTABLE_FIELDS.websites["website-platform"].id,
+      AIRTABLE_FIELDS.websites["digital-brand-category"].id,
+      AIRTABLE_FIELDS.websites["type-of-domain"].id,
+      AIRTABLE_FIELDS.websites["score-amp"].id,
+      AIRTABLE_FIELDS.websites["score-customer-centricity"].id,
+      AIRTABLE_FIELDS.websites["score-ga"].id,
+      AIRTABLE_FIELDS.websites["score-mobile-perf"].id,
+      AIRTABLE_FIELDS.websites["score-reqd-links"].id,
+      AIRTABLE_FIELDS.websites["score-uswds"].id,
+    ],
+    sort: [{ field: AIRTABLE_FIELDS.websites.website.id, direction: "asc" }],
+    // Selecting the first 3 records in Grid view:
+    view: "Sites w Evals",
+  })
+  .eachPage(
+    function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+
+      records.forEach(function (record) {
+        data.push(organizeResult(record));
+      });
+
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+    },
+    function done(err:any) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      mkdir('public/data/archive', { recursive: true }, (err:any) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      });
+      writeFile(
+        `public/data/archive/websites_${formattedDate()}.json`,
+        JSON.stringify(data),
+        (err: any) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        }
+      );
+      writeFile(`public/data/websites.json`, JSON.stringify(data), (err:any) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      });
+    }
+  );
+
+const organizeResult = function (record) {
+  return {
+    name: record.fields[AIRTABLE_FIELDS.websites.website.name],
+    office: record.fields[AIRTABLE_FIELDS.websites.office.name],
+    subOffice: record.fields[AIRTABLE_FIELDS.websites["sub-office"].name],
+    digitalBrandCategory:
+      record.fields[AIRTABLE_FIELDS.websites["digital-brand-category"].name],
+    typeOfDomain:
+      record.fields[AIRTABLE_FIELDS.websites["type-of-domain"].name],
+    //platform: platforms[getRandNo(7)],
+    datapointA: {
+      val: record.fields[
+        AIRTABLE_FIELDS.websites["score-customer-centricity"].name
+      ],
+      max: 1,
+    },
+    datapointB: {
+      val: record.fields[AIRTABLE_FIELDS.websites["score-reqd-links"].name],
+      max: 1,
+    },
+    datapointC: {
+      val: record.fields[AIRTABLE_FIELDS.websites["score-mobile-perf"].name],
+      max: 1,
+    },
+    datapointD: {
+      val: record.fields[AIRTABLE_FIELDS.websites["score-amp"].name],
+      max: 1,
+    },
+    datapointE: {
+      val: record.fields[AIRTABLE_FIELDS.websites["score-ga"].name],
+      max: 1,
+    },
+    datapointF: {
+      val: record.fields[AIRTABLE_FIELDS.websites["score-uswds"].name],
+      max: 1,
+    },
+  };
+};
+
